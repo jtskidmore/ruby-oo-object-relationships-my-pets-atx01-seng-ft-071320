21a4a008a11aaff088919356bda988c2f1499fb2
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-16 17:31:24 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,12 +2,17 @@ class Cat
 
   attr_reader :name
 
+  @@all = []
+
   def initialize (name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
+    self.class.all << self
   end
 
-  def 
+  def owner
+  end
 
   # code goes here
 end
