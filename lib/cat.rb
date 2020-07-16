e68d0523c56fd18e7a7253132417ca256f1904ea
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-16 17:29:19 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,19 +1,13 @@
 class Cat
 
   attr_reader :name
-  attr_accessor :owner, :mood
 
   def initialize (name, owner)
     @name = name
     @owner = owner
   end
 
-  def name
-    @name
-  end
+  def 
 
-  def owner
-    @owner
-  end
   # code goes here
 end
