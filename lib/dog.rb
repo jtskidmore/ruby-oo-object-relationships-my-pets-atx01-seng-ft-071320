aa3c0be424b521424846c664cdecac97eaede009
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-16 17:20:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,12 @@
 class Dog
 
+  attr_accessor :name, :owner
+
   def initialize (name, owner)
     @name = name
     @owner = owner
   end
 
-  def name
-    @name
-  end
-
-  def owner
-    @owner
-  end
+  
   # code goes here
 end
